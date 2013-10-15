f938371f9248f0dc39183a78cfb242f0ce97d883
JensenBan <jensenban@gmail.com>
2013-10-15 17:29:19 -0400

    GameMenu

---
+++ GameMenuUnity/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+		
+	void OnMouseEnter() {
+		guiText.material.color = Color.yellow;
+	}
+	void OnMouseExit(){
+		guiText.material.color = Color.white;	
+	}
+		
+	void onMouseDown(){		
+		
+		
+	}
+	// Use this for initialization
+	/*void Start () {
+	
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetMouseButton(0)){
+			Application.LoadLevel("InGame");
+		}
+	}
+	
+	
+}
