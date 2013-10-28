4d543b8de152cfae8829222b7f7536390c2649be
Venompaw <avatar_of_venom@hotmail.com>
2013-10-28 13:01:11 -0400

    Current build
    This is the current version

---
+++ Current Buildv1.0/Assets/Scripts/CreepSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepSpawner : MonoBehaviour 
+{
+	public GameObject prefab;
+	public int numberOfObjects = 20;
+	private float time;
+	public float spawnDelay;
+	private int i;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		time = Time.time;
+		i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (i < numberOfObjects && (Time.time - time) >= spawnDelay) 
+		{
+			i = i + 1;
+			time = Time.time;
+			Instantiate(prefab);
+			//private GameObject creep = (GameObject)Instantiate(prefab);
+			//creep.SendMessage("ApplyDamage", 1.0F);
+		}
+	}
+}
