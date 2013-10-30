19270fd116128edbb9921ec047730d8048b736c7
Venompaw <avatar_of_venom@hotmail.com>
2013-10-30 13:30:01 -0400

    Version1.01
    Version1.01 with assets
    
    The assets contains everything, the version 1.01 is a game that can be
    played as soon as it's opened in unity

---
+++ Version1.01/Assets/Scripts/HPBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPBehaviourScript : MonoBehaviour 
+{
+	public int hp;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		guiText.text = hp.ToString();
+	}
+	
+	void DamageHP()
+	{
+		hp = hp - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		guiText.text = hp.ToString()+" Lives";
+	}
+}
