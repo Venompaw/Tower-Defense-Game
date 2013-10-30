eaa39dacc23dae08e8b8ca662775400a027e639b
JensenBan <jensenban@gmail.com>
2013-10-30 12:54:36 -0400

    TowerDefenseMenuVersion1.02
    This update includes gold display with a button and an error message
    when not enough gold.

---
+++ Tower-Defense-Game-Version1.02/GameMenuUnity/Assets/Scripts/MainGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnGUI() {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "Start Round"))
+		{
+            BroadcastMessage("StartRound");
+		}
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Tower-Defense-Game-Version1.02/TowerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerScript : MonoBehaviour {
+	public object creep;
+	public GameObject prefab;
+	public Transform bullet;
+	public int bulletName = 0;
+	private Hashtable creepHash = new Hashtable();
+	private int isActive;
+	// Use this for initialization
+	void Start () {
+		//prefab =  Resources.Load ("Sphere") as GameObject;
+		isActive = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, 4);
+		
+        int i = 0;
+        while (i < hitColliders.Length) {
+			
+			if (isActive == 0)
+			{
+				
+				GameObject myObject = Instantiate(prefab, new Vector3(0, 2, 0),Quaternion.identity) as GameObject;
+				myObject.SendMessage("TestMethod", hitColliders[i].gameObject.name);
+				bulletName++;
+				isActive++;
+				
+				
+				
+			}
+			
+			
+			//test1.transform.position = new Vector3(test1.transform.position.x + 1, test1.transform.position.y, test1.transform.position.z);
+            i++;
+        }
+		if (isActive != 0)
+			{
+				if (isActive > 50 || isActive < 0)
+					isActive = 0;
+				else
+					isActive++;
+			}
+		
+	}
+	
+}
