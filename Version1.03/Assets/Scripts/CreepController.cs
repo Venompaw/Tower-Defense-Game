ced2d2e0d3d5f0add759eadd40458a1e7449814d
Venompaw <avatar_of_venom@hotmail.com>
2013-11-04 13:53:21 -0500

    Version 1.03
    Contains game over menu and varied beastiary.

---
+++ Version1.03/Assets/Scripts/CreepController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CreepController : MonoBehaviour 
+{
+	public GameObject markers; 
+	private Transform startMarker;
+    private Transform endMarker;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+    public float smooth = 5.0F;
+	public float health = 10.0F;
+	private int previousMarker;
+	public float coins = 10;
+	
+    void Start() 
+	{
+		previousMarker = 0;
+		int next = previousMarker + 1;
+		startMarker = markers.transform.Find("Path Node " + previousMarker);
+		endMarker = markers.transform.Find("Path Node " + next);
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+	
+	void ApplyDamage(float damage)
+	{
+		health = health - damage;	
+	}
+	
+	void AddParent(Transform hitPoints)
+	{
+		if (transform.parent == null)
+		{
+			transform.parent = hitPoints;
+		}
+	}
+	
+	void ModifyHealth(float mod)
+	{
+		health = health * mod;
+	}
+	
+	void ModifySpeed(float mod)
+	{
+		speed = speed * mod;
+	}
+	void ModifyCoins(float mod)
+	{
+		coins = coins * mod;
+	}
+			
+	
+    void Update() 
+	{
+		if (health <= 0)
+		{
+			//give player coins
+			GameObject.Destroy(gameObject);
+		}
+		
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+		
+		if (fracJourney >= 1.0F)
+		{
+			try
+			{
+			previousMarker = previousMarker +1;
+			int next = previousMarker + 1;
+			startMarker = markers.transform.Find("Path Node " + previousMarker);
+			endMarker = markers.transform.Find("Path Node " + next);
+        	startTime = Time.time;
+        	journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+			}
+			catch(Exception)
+			{
+				SendMessageUpwards("DamageHP");
+				GameObject.Destroy(gameObject);
+			}
+		}
+    }
+}
---
+++ Version1.03/Assets/Scripts/CreepSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepSpawner : MonoBehaviour 
+{
+	public GameObject[] prefab;
+	public int[] amountInRound;
+	public float[] spawnDelay;
+	private float[] time;
+	private int[] roundCounter;
+	private GameObject creep;
+	public Transform hitPoints;
+	private int round;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		round = 0;
+		time = new float[prefab.Length];
+		roundCounter = new int[prefab.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		int i = 0;
+		while (i < round)
+		{
+			if (roundCounter[i] < amountInRound[i] && (Time.time - time[i]) >= spawnDelay[i]) 
+			{
+				time[i] = Time.time;
+				roundCounter[i] = roundCounter[i] + 1;
+				creep = (GameObject)Instantiate(prefab[i]);
+				creep.SendMessage("AddParent", hitPoints);
+			}
+			else
+			{
+				i = i + 1;
+			}
+		}
+	}
+	
+	void StartRound()
+	{
+		time[round] = Time.time;
+		round = round + 1;
+	}
+}
