e51cca511d05d5ccc10795446335e687efad3909
Venompaw <avatar_of_venom@hotmail.com>
2013-11-04 14:05:06 -0500

    Version 1.04
    with assets

--- Version1.04/Assets/Scripts/GoldBehaviour.cs
+++ Version1.04/Assets/Scripts/GoldBehaviour.cs
@@ -4,29 +4,30 @@ using System.Collections;
 public class GoldBehaviour : MonoBehaviour {
 	
 	public float gold;
+	
 	public Transform errorText;
 	// Use this for initialization
 	void Start () {
 		guiText.text = gold.ToString();
 	}
-	
-	void OnGUI(){
 		
-		    if (GUI.Button(new Rect(1300, 230, 50, 50), "Tower"))
-    {
-			if(gold>300)
+	
+	
+	bool GoldDecrement(float goldDecrease)
+	{
+		if(gold>goldDecrease)
 			{
-				gold = gold - 300;
+				gold = gold-goldDecrease;
+			return true;
 			}
-			else
-			{
-				//errorText.guiText.text="Not enough gold!";
-				errorText.SendMessage("DisplayMessage", "Not enough gold!");
-			}
-     
-    }
+			return false;
+		
 	}
 	
+	void GoldIncrement(float goldIncrease)
+	{
+		gold = gold+goldIncrease;
+	}
 	// Update is called once per frame
 	void Update () {
 		
