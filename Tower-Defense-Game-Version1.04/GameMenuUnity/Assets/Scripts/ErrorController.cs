e6beb8199ec24d7bf66f3aec2af078f2b73c7c40
JensenBan <jensenban@gmail.com>
2013-11-04 13:35:34 -0500

    Tower Defense Game Version1.04
    Created methods for incrementing and decrementing gold.

---
+++ Tower-Defense-Game-Version1.04/GameMenuUnity/Assets/Scripts/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ErrorController : MonoBehaviour {
+	public float duration = 0.2f;
+	private float startTime;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((Time.time - startTime) >= duration)
+		{
+			guiText.text="";
+		}
+	}
+	
+	void DisplayMessage (string message)
+	{
+		guiText.text= message;
+		startTime = Time.time;
+	}
+}
---
+++ Tower-Defense-Game-Version1.04/GameMenuUnity/Assets/Scripts/GoldBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoldBehaviour : MonoBehaviour {
+	
+	public float gold;
+	
+	public Transform errorText;
+	// Use this for initialization
+	void Start () {
+		guiText.text = gold.ToString();
+	}
+	
+	void OnGUI(){
+		
+		    if (GUI.Button(new Rect(1300, 230, 50, 50), "Tower"))
+    {
+			if(GoldDecrement(300))
+			{
+				//Place tower
+			}
+			else
+			{
+				//errorText.guiText.text="Not enough gold!";
+				errorText.SendMessage("DisplayMessage", "Not enough gold!");
+			}
+     
+    }
+	}
+	
+	bool GoldDecrement(float goldDecrease)
+	{
+		if(gold>goldDecrease)
+			{
+				gold = gold-goldDecrease;
+			return true;
+			}
+			return false;
+		
+	}
+	
+	void GoldIncrement(float goldIncrease)
+	{
+		gold = gold+goldIncrease;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+		guiText.text = gold.ToString();
+	
+	}
+}
