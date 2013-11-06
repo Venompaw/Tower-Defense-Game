ade89a5e6ef24294d996a548c1a4e79af42070f3
Venompaw <avatar_of_venom@hotmail.com>
2013-11-06 13:19:52 -0500

    Version 1.05 with assets
    Changes:
    User should be able to gain gold when creep dies.
    User should be able to select difficulty which affects creep strength.

---
+++ Version1.05/Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevel : MonoBehaviour {
+	
+	void OnMouseEnter()
+	{
+		guiText.material.color = Color.yellow;
+		
+	}
+	void OnMouseExit()
+	{
+		guiText.material.color = Color.white;
+	}
+
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel("InGame");
+	}
+	
+}
