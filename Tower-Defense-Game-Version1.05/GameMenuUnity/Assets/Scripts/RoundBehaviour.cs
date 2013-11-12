1dda5e6e168812086fcca0240cd4eb6ebf0ca5e0
JensenBan <jensenban@gmail.com>
2013-11-12 12:43:53 -0500

    Tower Defense Game Version 1.05
    Added round counter and score display.

---
+++ Tower-Defense-Game-Version1.05/GameMenuUnity/Assets/Scripts/RoundBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoundBehaviour : MonoBehaviour {
+	
+	private int round;
+	// Use this for initialization
+	void Start () {
+	guiText.text = round.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	guiText.text = round.ToString();
+	}
+}
---
+++ Tower-Defense-Game-Version1.05/GameMenuUnity/Assets/Scripts/ScoreBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreBehaviour : MonoBehaviour {
+	
+	public float score;
+	
+
+	// Use this for initialization
+	void Start () {
+		guiText.text = "Score: " + score.ToString("F0");
+	
+	}
+	bool ScoreDecrement(float scoreDecrease)
+	{
+		if(score>0)
+		{
+			score = score-scoreDecrease;
+			return true;
+		}
+		return true;
+	}
+	void OnGUI()
+	{
+		if(ScoreDecrement(0.1f))
+		{
+			
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+			
+		
+		guiText.text = "Score: " + score.ToString("F0");
+	}
+}
