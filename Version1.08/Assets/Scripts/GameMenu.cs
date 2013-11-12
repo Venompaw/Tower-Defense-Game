0415a90fbde2f8b0b503977ed7b1fe93d544ec9c
Venompaw <avatar_of_venom@hotmail.com>
2013-11-12 15:22:13 -0500

    Version 1.08 with Assets
    Includes round counter, an in progress score counter.

---
+++ Version1.08/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+	public static int difficulty;
+		
+	void OnMouseEnter() {
+		guiText.material.color = Color.yellow;
+	}
+	void OnMouseExit(){
+		guiText.material.color = Color.white;	
+	}
+		
+	void OnMouseDown()
+	{		
+		switch (guiText.text)
+		{
+		case "Easy":
+			difficulty = 0;
+			break;
+		case "Medium":
+			difficulty = 1;
+			break;
+		case "Hard":
+			difficulty = 2;
+			break;
+		}
+		Application.LoadLevel("InGame");
+		
+	}
+	
+}
---
+++ Version1.08/Assets/Scripts/HPBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPBehaviourScript : MonoBehaviour 
+{
+	public int hp;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		guiText.text = "Lives: " + hp.ToString();
+	}
+	
+	void DamageHP()
+	{
+		hp = hp - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(hp <= 0)
+		{Application.LoadLevel("GameOver");
+		}
+		
+		guiText.text = "Lives: " + hp.ToString();
+	}
+}
