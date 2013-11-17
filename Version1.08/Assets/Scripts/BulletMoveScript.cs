d6ebc79aecb6842c55e13e605ef8bf18024a4011
jhirst <lord.fezar@gmail.com>
2013-11-17 18:51:10 -0500

    Towers with scene
    Towers updated with scene, toggleable

---
+++ Version1.08/Assets/Scripts/BulletMoveScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMoveScript : MonoBehaviour {
+	
+	//private string creepID;
+	public GUIText guiText;
+	private GameObject creepObject;
+	// Use this for initialization
+	void Start () {
+		
+		/*   B instanceOfB = GameObject.Find("Name of GameObject to which B is attached").GetComponent<B>();
+        		instanceOfB.printStuff("this is b's method");
+   				 }
+					
+					*/
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(creepObject == null)
+		{
+			Destroy(gameObject);
+		}
+		transform.position = Vector3.MoveTowards(transform.position, creepObject.transform.position, 
+			5.0f * Time.deltaTime);
+		
+		if(transform.position == creepObject.transform.position)
+		{
+			creepObject.SendMessage("ApplyDamage", 20.0f);
+			Destroy(gameObject);
+		}
+		
+		//ApplyDamage
+		//Destroy(this);
+	}
+	
+	public void TestMethod(GameObject creepObject)
+	{	//Accept a gameObject
+		this.creepObject = creepObject;
+	}
+}
---
+++ Version1.08/Assets/Scripts/TowerSpawnScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerSpawnScript : MonoBehaviour {
+	
+	public GameObject towerPrefab;
+	public Camera camera;
+	public bool testStop;
+	public GameObject ground;
+	public GUIText text1;
+	public GUIText text2;
+	bool isActive;
+	GameObject towerObject;
+	Vector3 objectPos;
+	
+	// Use this for initialization
+	void Start () {
+	towerObject = Instantiate(towerPrefab, objectPos, Quaternion.identity) as GameObject;
+			isActive = true;
+		testStop = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(testStop == false)
+		{
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = -camera.transform.position.z;
+		text1.text = mousePos.ToString();
+       	objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+		objectPos.y = 0;
+		text2.text = isActive.ToString();
+		if (isActive == false)
+		{
+				if(towerObject != null)
+			towerObject.BroadcastMessage("FireActive");
+      		towerObject = Instantiate(towerPrefab, objectPos, Quaternion.identity) as GameObject;
+			isActive = true;
+		}
+		else
+		{
+			if(towerObject != null)
+			towerObject.BroadcastMessage("FollowMouse", objectPos);
+			
+		}
+		}
+		else
+		{
+			if (towerObject != null)
+			towerObject.BroadcastMessage("DestroyTower");
+		}
+	}
+	
+	public void PlaceTower(bool isActive)
+	{
+		this.isActive = isActive;
+	}
+	
+	public void TestStop(bool testStop)
+	{
+		this.testStop = testStop;
+		isActive = testStop;
+	}
+}
