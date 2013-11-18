073af34803bcfd4681a2ce22b4bdda3245625506
jhirst <lord.fezar@gmail.com>
2013-11-18 03:57:14 -0500

    Reupload

---
+++ TDProj/Jeff's Updates/CreepMoveScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepMoveScript : MonoBehaviour {
+	// Use this for initialization
+	public float health = 10F;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
+		if (health <= 0)
+		{
+			//give player coins
+			GameObject.Destroy(gameObject);
+		}
+		
+		
+	}
+	
+	void ApplyDamage(float damage)
+	{
+		health = health - damage;	
+	}
+}
---
+++ TDProj/Jeff's Updates/TowerDestroyScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerDestroyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void DestroyTower()
+	{
+		Destroy(gameObject);	
+	}
+}
