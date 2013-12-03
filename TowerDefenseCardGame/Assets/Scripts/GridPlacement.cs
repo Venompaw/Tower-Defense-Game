e77f0acfb41458c6646d66c00c2987cc57b72ee8
AndrewMackle <andrewmackle@hotmail.com>
2013-12-03 09:37:23 -0500

    User should be able to place towers on a grid

---
+++ TowerDefenseCardGame/Assets/Scripts/GridPlacement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridPlacement : MonoBehaviour 
+{
+	private Transform selectedTower;
+	private bool isEmpty;
+	public GameObject cam;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		isEmpty = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	void OnMouseUp () 
+	{
+		if (isEmpty && selectedTower != null)
+		{
+			selectedTower.position = transform.position;
+			isEmpty = !isEmpty;
+			ClearTower();
+		}
+	}
+	
+	void OnMouseOver ()
+	{
+		cam.BroadcastMessage("SetGridFull", !isEmpty);
+	}
+	
+	public void SetTower (Transform tower)
+	{
+		selectedTower = tower;
+	}
+	
+	public void ClearTower ()
+	{
+		selectedTower = null;
+	}
+}
